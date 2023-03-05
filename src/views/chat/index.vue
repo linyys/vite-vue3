b05099dfb50e222b8a952652663126c06fb3f00b
linyys <1398809407@qq.com>
2023-03-05 22:11:22 +0800

    style(custom): 1

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,33 +1,102 @@
 <template>
   <div>
     <a-card>
-      <a-button @click="join">加入</a-button>
-      <a-input v-model="uid" />
-      <a-button @click="conn">连接</a-button>
-      <a-input v-model="c_uid" />
+      <div
+        v-for="item in user_list"
+        :key="item"
+        style="cursor: pointer; margin: 10px 0"
+        @click="conn(item)"
+      >
+        <div v-if="item !== uid">{{ item }}</div>
+      </div>
+      <a-input v-model="input" />
+      <a-button @click="send">send</a-button>
     </a-card>
   </div>
 </template>
 <script lang="ts" setup>
-import { chat_conn, chat_join } from "@/api/chat"
-import { ref } from "vue"
+import { ref, reactive, onMounted } from "vue"
+import { io } from "socket.io-client"
+import { Ref } from "vue"
 
-const c_uid = ref("")
-const uid = ref("")
-
-const conn = async () => {
-  const { data: res } = await chat_conn({ uid: c_uid.value, c_uid: "123" })
-  console.log(res)
+const uid = new Date().getTime().toString()
+const user_list: Ref = ref([])
+const socket = io("http://127.0.0.1:4000", { autoConnect: false })
+const input = ref("")
+const localConnection = new RTCPeerConnection({})
+let dc: RTCDataChannel
+localConnection.onsignalingstatechange = function () {
+  console.log("Signaling state: " + localConnection.signalingState)
 }
-const join = async () => {
-  const { data: res } = await chat_join({ uid: uid.value })
-  console.log(res)
-}
-const create_conn = async (config: any) => {
-  // 创建本地 RTCPeerConnection 对象
-  const localConnection = new RTCPeerConnection({})
+const conn = async (c_uid: string) => {
   const offer = await localConnection.createOffer({})
   await localConnection.setLocalDescription(offer)
+  socket.emit("send", { offer, c_uid, uid, state: "c_offer" }, (data: any) => {
+    console.log("发送", data)
+  })
 }
+
+const respond = async (c_uid: any) => {
+  const answer = await localConnection.createAnswer({})
+  await localConnection.setLocalDescription(answer)
+  socket.emit(
+    "send",
+    { answer, c_uid, uid, state: "c_answer" },
+    (data: any) => {
+      console.log(data)
+    }
+  )
+}
+
+const join = async () => {
+  socket.emit("join", { uid }, (data: any) => {
+    console.log("加入")
+  })
+  socket.on(uid, async (data) => {
+    if (data.state == "c_offer") {
+      await accept_offer(data)
+      await respond(data.uid)
+      ready()
+    } else if (data.state == "c_answer") {
+      await accept_answer(data.answer)
+      ready()
+    }
+  })
+  socket.on("joined", (data) => {
+    user_list.value.push(data)
+  })
+}
+
+const ready = () => {
+  localConnection.addEventListener("icecandidate", (e) => {
+    localConnection.addIceCandidate(e.candidate as any)
+  })
+  console.log(localConnection)
+
+  dc = localConnection.createDataChannel("chat")
+  dc.onopen = () => {
+    console.log("已打开")
+  }
+  dc.onmessage = (e) => {
+    console.log(e)
+  }
+}
+
+const send = () => {
+  dc.send(input.value)
+}
+
+const accept_offer = async (data: any) => {
+  await localConnection.setRemoteDescription(data.offer)
+}
+
+const accept_answer = async (answer: any) => {
+  await localConnection.setRemoteDescription(answer)
+}
+
+onMounted(async () => {
+  socket.connect()
+  await join()
+})
 </script>
 <style scoped></style>
